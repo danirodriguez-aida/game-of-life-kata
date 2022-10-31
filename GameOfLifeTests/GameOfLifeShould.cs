a6827920f29a8499c4f17d8e7cf83a85c3b9eac1
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-31 07:27:22 +0000

    red test a_dead_cell_with_dead_neighbors_stays_dead

--- GameOfLifeTests/GameOfLifeShould.cs
+++ GameOfLifeTests/GameOfLifeShould.cs
@@ -1,12 +1,24 @@
+using FluentAssertions;
+
 namespace GameOfLifeTests {
     public class GameOfLifeShould {
-        [SetUp]
-        public void Setup() {
-        }
-
+        
         [Test]
-        public void Test1() {
-            Assert.Pass();
+        public void a_dead_cell_with_dead_neighbors_stays_dead()
+        {
+            var initalBoard = new bool[3, 3];
+
+            var board = GameOfLife.NextGeneration(initalBoard);
+
+            board[1, 1].Should().BeFalse();
+        }
+    }
+
+    public class GameOfLife
+    {
+        public static bool[,] NextGeneration(bool[,] board)
+        {
+            throw new NotImplementedException();
         }
     }
 }
