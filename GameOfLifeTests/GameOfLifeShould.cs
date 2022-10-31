52c7ab1f3d7f6f59bf29e9d9c6e4b93db9a2011d
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-31 07:15:28 +0000

    Add project files.

---
+++ GameOfLifeTests/GameOfLifeShould.cs
@@ -0,0 +1,12 @@
+namespace GameOfLifeTests {
+    public class GameOfLifeShould {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
