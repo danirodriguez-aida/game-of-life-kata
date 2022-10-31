8187bd346c4109f3e610a973c1174d99c6f6915e
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-31 07:38:21 +0000

    red test a_living_cell_with_less_than_two_living_neighboring_dies

--- GameOfLifeTests/GameOfLifeShould.cs
+++ GameOfLifeTests/GameOfLifeShould.cs
@@ -12,6 +12,17 @@ namespace GameOfLifeTests {
 
             board[1, 1].Should().BeFalse();
         }
+
+        [Test]
+        public void a_living_cell_with_less_than_two_living_neighboring_dies()
+        {
+            var initalBoard = new bool[3, 3];
+            initalBoard[1, 1] = true;
+
+            var board = GameOfLife.NextGeneration(initalBoard);
+
+            board[1, 1].Should().BeFalse();
+        }
     }
 
     public class GameOfLife
