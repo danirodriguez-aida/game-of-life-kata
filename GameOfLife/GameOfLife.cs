575587f97b2e86595ee2e942c29ac1e9e1d3e4ba
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-11-03 07:34:50 +0000

    green test a_living_cell_with_less_than_two_living_neighboring_dies for 0,2

--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -15,6 +15,10 @@ public class GameOfLife {
         var neighborsFor22 = board.GetNeighbors(Position.In(2, 2));
         var aliveNeighbors22 = neighborsFor22.Count(n => n.IsAlive());
         if (aliveNeighbors22 < 2) board.SetCellToDead(Position.In(2, 2));
+
+        var neighborsFor02 = board.GetNeighbors(Position.In(0, 2));
+        var aliveNeighbors02 = neighborsFor02.Count(n => n.IsAlive());
+        if (aliveNeighbors02 < 2) board.SetCellToDead(Position.In(0, 2));
     }
 
     public bool IsCellAlive(Position position) {
